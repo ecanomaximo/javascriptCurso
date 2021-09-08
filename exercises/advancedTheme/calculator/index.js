6bcfe539c86190a689ccf1929053aa617f124610
Eduardo Cano <e.maximocano@gmail.com>
2021-09-08 12:00:31 -0300

    refactoring calculator

--- exercises/advancedTheme/calculator/index.js
+++ exercises/advancedTheme/calculator/index.js
@@ -1,74 +1,52 @@
-function createCalculator() {
-    return {
-        display: document.querySelector('.calculatorDisplay'),
+function Calculator() {
+    this.display = document.querySelector('.calculatorDisplay');
+    
+    this.start = () => {
+        this.clicksCapture();
+        this.enterCapture();
+    }
 
-        start() {
-            this.clickButtons();
-            this.pressEnter();
-        },
-
-        pressEnter() {
-            this.display.addEventListener('keyup', event => {
-                if(event.keyCode === 13) {
-                    this.doAccount();
-                }
-            });
-        },
-
-        delDisplayNum() {
-            this.display.value = this.display.value.slice(0, -1)
-        },
-
-        clearDisplay() {
-            this.display.value = '';
-        },
-
-        doAccount() {
-            let result = this.display.value; 
-
-            try {
-                result = eval(result);
-
-                if(!result) {
-                    alert('Invalid Account');
-                    return;
-                }
-
-                this.display.value= result;
-            } catch(error) {
-                if(!result) {
-                    alert('Invalid Account');
-                    return;
-                }
+    this.enterCapture = () => {
+        this.display.addEventListener('keypress', event => {
+            if(event.keyCode === 13) {
+                this.equal();
             }
-        },
+        });
+    }
 
-        clickButtons() {
-            document.addEventListener('click', event => {
-                const elementEvent = event.target;
-
-                if(elementEvent.classList.contains('btn-num')) {
-                    this.btnForDisplay(elementEvent.innerText);
-                }
-
-                if(elementEvent.classList.contains('btn-clear')) {
-                    this.clearDisplay();
-                }
-
-                if(elementEvent.classList.contains('btn-del')) {
-                    this.delDisplayNum();
-                }
-
-                if(elementEvent.classList.contains('btn-equal')){
-                    this.doAccount();
-                }
-            });
-        },
-        btnForDisplay(value) {
-            this.display.value += value;
-        }
+    this.clicksCapture = () => {
+        document.addEventListener('click', event => {
+            const element = event.target;
+            if(element.classList.contains('btn-num')) this.addDisplayNumber(element);
+            if(element.classList.contains('btn-clear')) this.clear();
+            if(element.classList.contains('btn-del')) this.del();
+            if(element.classList.contains('btn-equal')) this.equal();
+        });
     };
+
+    this.equal = () => {
+        try {
+            const captureAccount = eval(this.display.value);
+
+            if(!captureAccount) {
+                alert('Invalid');
+                return
+            }
+
+            this.display.value = captureAccount;
+        } catch(error) {alert(error.message);
+            return;
+        }
+    }
+
+    this.addDisplayNumber = element => {
+        this.display.value += element.innerText;
+        this.display.focus();
+    };
+    this.clear = () => this.display.value = '';
+    this.del = () => this.display.value.slice(0, -1);
+
 }
 
-const calculator = createCalculator();
+const calculator = new Calculator();
 calculator.start();
--- exercises/advancedTheme/functions.md
+++ exercises/advancedTheme/functions.md
@@ -1,5 +1,3 @@
-function hoisting = a engine dojavascript eleva as declarações de funções e variaveis declaradas pro topo do javascript.
-
     Funções:
 Funções são objetos de primeira classe, e isso quer dizer que a função pode ser tratada como um dado.
 
@@ -7,3 +5,8 @@ function expression = criar uma variavel que recebe dentro dela uma função, as
 
 arrow function = é uma declaração de função mais curta.
 
+function constructor - constrói uma função com objetos. começa com letra maiúscula, obrigatóriamente com a palavra new
+
+function factory - fabrica uma função com Objetos.
+
+function hoisting = a engine do javascript eleva as declarações de funções e variaveis declaradas pro topo do javascript.
